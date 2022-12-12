cfd6d993f9d4bee8cc0efc4ddf5e318c2285fdf1
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2022-12-12 19:01:27 +0200

    exported setup function

--- lua/strict/init.lua
+++ lua/strict/init.lua
@@ -1,3 +1,21 @@
+local strict = {}
+local default_config = {
+    excluded_filetypes = nil, -- { 'text', 'markdown', 'html' }
+    highlight_group = 'DiffDelete',
+    deep_nesting = {
+        highlight = true,
+        depth_limit = 3, -- 4
+        ignored_characters = nil -- '\'".'
+    },
+    overlong_lines = {
+        highlight = true,
+        length_limit = 80
+    },
+    trailing_whitespace = {
+        highlight = true
+    }
+}
+
 local function is_excluded_filetype(excluded_filetypes)
     local buffer_filetype = vim.bo.filetype
     if type(excluded_filetypes) == 'string' then
@@ -10,7 +28,7 @@ local function is_excluded_filetype(excluded_filetypes)
     return false
 end
 
-local function match_nest(highlight_group, nest_limit, nest_ignore_chars)
+local function match_deep_nesting(highlight_group, nest_limit, nest_ignore_chars)
     local indent_size = vim.bo.shiftwidth
     local nest_regex = string
         .format('^\\s\\{%s}\\zs\\s\\+\\(\\s*[%s]\\)\\@!',
@@ -24,7 +42,7 @@ local function match_trailing_whitespace(highlight_group)
     vim.fn.matchadd(highlight_group, trailing_whitespace_regex, -1)
 end
 
-local function match_line_length(highlight_group, line_length_limit)
+local function match_overlong_lines(highlight_group, line_length_limit)
     local line_length_regex = string
         .format('\\%s>%sv.\\+', '%', line_length_limit)
     vim.fn.matchadd(highlight_group, line_length_regex, -1)
@@ -34,30 +52,26 @@ local strict_augroup = vim.api.nvim_create_augroup('strict', {
     clear = true
 })
 
-vim.api.nvim_create_autocmd('BufEnter', {
-    group = strict_augroup,
-    callback = function()
-        local excluded_filetypes = { 'text', 'markdown', 'html' }
-        local highlight_group = 'DiffDelete'
-
-        local highlight_deep_nesting = true
-        local nesting_limit = 4
-        local nesting_ignored_chars = '\'".'
-
-        local highlight_overlong_lines = true
-        local line_length_limit = 80
-
-        local highlight_trailing_whitespace = true
-
-        if is_excluded_filetype(excluded_filetypes) then return end
-        if highlight_trailing_whitespace then
-            match_trailing_whitespace(highlight_group)
+function strict.setup(config)
+    config = config or default_config
+    vim.api.nvim_create_autocmd('BufEnter', {
+        group = strict_augroup,
+        callback = function()
+            if is_excluded_filetype(config.excluded_filetypes) then return end
+            if config.trailing_whitespace.highlight then
+                match_trailing_whitespace(config.highlight_group)
+            end
+            if config.overlong_lines.highlight then
+                match_overlong_lines(config.highlight_group,
+                    config.overlong_lines.length_limit)
+            end
+            if config.deep_nesting.highlight then
+                match_deep_nesting(config.highlight_group,
+                    config.deep_nesting.depth_limit,
+                    config.deep_nesting.ignored_characters)
+            end
         end
-        if highlight_overlong_lines then
-            match_line_length(highlight_group, line_length_limit)
-        end
-        if highlight_deep_nesting then
-            match_nest(highlight_group, nesting_limit, nesting_ignored_chars)
-        end
-    end
-})
+    })
+end
+
+return strict
