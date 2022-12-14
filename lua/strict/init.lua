6de80a9c0279de65ad16adbf242c11546d5353e2
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2022-12-14 14:17:56 +0200

    clear (only nvim-strict) matches when leaving buffer

--- lua/strict/init.lua
+++ lua/strict/init.lua
@@ -36,18 +36,19 @@ local function highlight_deep_nesting(highlight_group, depth_limit,
         .format('^\\s\\{%s}\\zs\\s\\+\\(\\s*[%s]\\)\\@!',
             depth_limit * indent_size,
             ignored_characters or '')
-    vim.fn.matchadd(highlight_group, nest_regex, match_priority)
+    return vim.fn.matchadd(highlight_group, nest_regex, match_priority)
 end
 
 local function highlight_trailing_whitespace(highlight_group)
     local trailing_whitespace_regex = '\\s\\+$\\|\\t'
-    vim.fn.matchadd(highlight_group, trailing_whitespace_regex, match_priority)
+    return vim.fn.matchadd(highlight_group, trailing_whitespace_regex,
+        match_priority)
 end
 
 local function highlight_overlong_lines(highlight_group, line_length_limit)
     local line_length_regex = string
         .format('\\%s>%sv.\\+', '%', line_length_limit)
-    vim.fn.matchadd(highlight_group, line_length_regex, match_priority)
+    return vim.fn.matchadd(highlight_group, line_length_regex, match_priority)
 end
 
 local function contains(table, string)
@@ -68,23 +69,41 @@ local function is_included_filetype(included_filetypes, excluded_filetypes)
     return true
 end
 
-local function autocmd_callback(config)
-    vim.fn.clearmatches()
-    if not is_included_filetype(config.included_filetypes,
-        config.excluded_filetypes) then return end
+local function delete_matches(matches)
+    for _, match in ipairs(matches) do
+        vim.fn.matchdelete(match)
+    end
+    return true
+end
+
+local function highlight(config)
+    local matches = {}
     if config.trailing_whitespace.highlight then
-        highlight_trailing_whitespace(config
-            .trailing_whitespace.highlight_group)
+        table.insert(matches, highlight_trailing_whitespace(
+            config.trailing_whitespace.highlight_group))
     end
     if config.overlong_lines.highlight then
-        highlight_overlong_lines(config.overlong_lines.highlight_group,
-            config.overlong_lines.length_limit)
+        table.insert(matches, highlight_overlong_lines(
+            config.overlong_lines.highlight_group,
+            config.overlong_lines.length_limit))
     end
     if config.deep_nesting.highlight then
-        highlight_deep_nesting(config.deep_nesting.highlight_group,
+        table.insert(matches, highlight_deep_nesting(
+            config.deep_nesting.highlight_group,
             config.deep_nesting.depth_limit,
-            config.deep_nesting.ignored_characters)
+            config.deep_nesting.ignored_characters))
     end
+    vim.api.nvim_create_autocmd('BufLeave', {
+        group = strict_augroup,
+        buffer = 0,
+        callback = function() return delete_matches(matches) end
+    })
+end
+
+local function autocmd_callback(config)
+    if not is_included_filetype(config.included_filetypes,
+        config.excluded_filetypes) then return end
+    highlight(config)
 end
 
 local function override_config(default, user)
@@ -101,9 +120,7 @@ function strict.setup(user_config)
     local config = override_config(default_config, user_config)
     vim.api.nvim_create_autocmd('BufEnter', {
         group = strict_augroup,
-        callback = function()
-            autocmd_callback(config)
-        end
+        callback = function() autocmd_callback(config) end
     })
 end
 
