e9b7fd869af765a725e1de71210f17dcd5efeb2f
Benjas333 <seston30@gmail.com>
2026-04-08 03:50:40 -0600

    Removed misleading comments

--- lua/strict/init.lua
+++ lua/strict/init.lua
@@ -53,8 +53,8 @@ local function highlight_deep_nesting(
     local indent_limit = depth_limit * shift_width
 
     local regex = string.format(
-        '^\\(\\t{%s}\\|\\s\\{%s}\\)' .. -- Matches lines starting with indentation
-        '\\zs\\s\\+',              -- Highlights the rest of the line
+        '^\\(\\t{%s}\\|\\s\\{%s}\\)' ..
+        '\\zs\\s\\+',
         depth_limit,
         indent_limit
     )
